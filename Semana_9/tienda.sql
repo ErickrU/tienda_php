74b9772b87eaf4e2d9c0d0f93767f6278ccddce0
Erick Alexander Rangel Urbina <65713712+ErickrU@users.noreply.github.com>
2020-12-01 17:00:15 -0600

    Update tienda.sql

--- Semana_9/tienda.sql
+++ Semana_9/tienda.sql
@@ -1,11 +1,11 @@
 create table if not exists productos(
-id_producto			integer primary key,
+id_producto			integer primary key AUTOINCREMENT,
 producto			  text(150) not null, 
 precio				  integer(10) not null,
 existencias			integer(10) not null);
 
 create table if not exists tickets(
-id_ticket 			        integer primary key,
+id_ticket 			        integer primary key AUTOINCREMENT,
 fecha				            date not null,
 hora_venta 			        time,
 cantidad_producto   		integer(100) not null,
