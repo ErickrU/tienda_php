8ec9aaa1232b54f03f753b8f5217ab4cc36412f0
Erick Alexander Rangel Urbina <65713712+ErickrU@users.noreply.github.com>
2020-12-01 16:45:14 -0600

    Create tienda.sql

---
+++ Semana_9/tienda.sql
@@ -0,0 +1,13 @@
+create table if not exists productos(
+id_producto			integer primary key,
+producto			  text(150) not null, 
+precio				  integer(10) not null,
+existencias			integer(10) not null);
+
+create table if not exists tickets(
+id_ticket 			        integer primary key,
+fecha				            date not null,
+hora_venta 			        time,
+cantidad_producto   		integer(100) not null,
+producto			          varchar(150) not null, 
+total_Producto 			    integer(10000) not null);
