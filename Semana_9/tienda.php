b6d82874b8bbf84b2e057ffdeab39f89e942aa2d
Erick Alexander Rangel Urbina <65713712+ErickrU@users.noreply.github.com>
2020-12-02 04:48:44 -0600

    Update tienda.php

--- Semana_9/tienda.php
+++ Semana_9/tienda.php
@@ -60,7 +60,7 @@
 		<p><label></br>Total a pagar</label>
         <input type="number" id="PGR" readonly="" placeholder="0.0"></label></p>
         <input type="reset" name="nuevacompra" value="Nueva compra" id="botonNueva" >
-        <input type="submit" value="pagar" name="nueva compra" OnClick="location.href='https://tiendaphp.erickalexandera.repl.co/Semana_8/ticket.php'">
+        <input type="submit" value="pagar" name="nueva compra" OnClick="location.href='https://tiendaphp.erickalexandera.repl.co/Semana_9/ticket.php'">
         
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
